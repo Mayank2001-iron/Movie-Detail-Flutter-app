7e30eda75713e09c7aab32926a6b340f6af3ac68
Mayank Kashyap <76560885+Mayank2001-iron@users.noreply.github.com>
2024-02-29 22:46:44 +0530

    Update MoviesModel.dart

--- lib/Models/MoviesModel.dart
+++ lib/Models/MoviesModel.dart
@@ -7,8 +7,7 @@ MoviesApiCall() async
   var response = await http.get(
     Uri.parse('https://movies-api14.p.rapidapi.com/movies'),
      headers: {
-    'X-RapidAPI-Key': '468f91dc8amshf41865004d75b0ap1a384fjsnf2ae337d8f13',
-    'X-RapidAPI-Host': 'movies-api14.p.rapidapi.com'
+    
   }
     );
     if(response.statusCode==200)
